8efd933cdff0c9eab080f8023e82f66f6d2eca46
VedikaSingh1 <vedikasingh3153@gmail.com>
2024-02-22 15:18:36 +0530

    Login Landing Page without CSS

--- src/components/Sections/login.js
+++ src/components/Sections/login.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login() {
-  console.log("Successful log in");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
+
+  const handleLogin = () => {
+    // Replace 'your_login_api_url' with your actual login API endpoint
+    const loginApiUrl =
+      "https://traderg-marketplace-backend.onrender.com/user/login";
+
+    fetch(loginApiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: username, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          setLoginError(true);
+          throw new Error("Login Failed"); // Handle specific error messages if needed
+        }
+        return response.json(); // Parse the JSON response
+      })
+      .then((data) => {
+        const jwtToken = data.data;
+        localStorage.setItem("jwtToken", jwtToken);
+        window.location.href = "Landingpage.html";
+      })
+      .catch((error) => {
+        console.error("Login Failed:", error);
+        setLoginError(true);
+      });
+  };
+
   return (
-    <div className="text">
-      {/* api password checking */}
-      <h1> LOGIN PAGE REACHED</h1>
+    <div>
+      <h1>LOGIN PAGE REACHED</h1>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button id="btn_signin" onClick={handleLogin}>
+        Sign In
+      </button>
+      {loginError && (
+        <p id="loginError">Login Failed. Please check your credentials.</p>
+      )}
     </div>
   );
 }
